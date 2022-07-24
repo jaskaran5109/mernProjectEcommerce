0a61e76943b516954ccb0737d424f231380086ed
jaskaran5109 <67426155+jaskaran5109@users.noreply.github.com>
2022-07-25 02:20:28 +0530

    Ecommerce

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './Store'
 import {positions,transitions,Provider as AlertProvider} from "react-alert"
 import AlertTemplate from 'react-alert-template-basic'
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const options = {
   timeout:5000,
@@ -13,13 +12,13 @@ const options = {
   transitions:transitions.SCALE
 }
 
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <AlertProvider template={AlertTemplate} {...options}>
-
-    <App />
+      <App />
     </AlertProvider>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 
